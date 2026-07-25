fd2635a23152f7412bcf4d276baab6f25a76a699
jmacwan <45224645+jmacwan@users.noreply.github.com>
2026-07-25 11:34:00 -0400

    Add Product model with properties

--- EcommerceApp.Api/Models/Product.cs
+++ EcommerceApp.Api/Models/Product.cs
@@ -1,1 +1,12 @@
-
+namespace ECommerceApp.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = "";
+        public int Stock { get; set; }
+    }
+}

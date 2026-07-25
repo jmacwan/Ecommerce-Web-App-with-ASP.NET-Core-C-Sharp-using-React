8e48becee6fa8d41987efabffa442a228e3da8e5
jmacwan <45224645+jmacwan@users.noreply.github.com>
2026-07-25 11:34:21 -0400

    Add CartItem model with properties

--- EcommerceApp.Api/Models/CartItem.cs
+++ EcommerceApp.Api/Models/CartItem.cs
@@ -1,1 +1,12 @@
+namespace ECommerceApp.Api.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = "";
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
 
+        public Product? Product { get; set; }
+    }
+}
